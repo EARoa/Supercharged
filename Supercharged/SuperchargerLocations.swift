a431fb9347d1453288bf9c170758e733d6bb0a1d
Efrain <jesusayllonroa@gmail.com>
2016-08-07 16:41:22 -0500

    Displaying all locations

---
+++ Supercharged/SuperchargerLocations.swift
@@ -0,0 +1,34 @@
+//
+//  SuperchargerLocations.swift
+//  Supercharged
+//
+//  Created by Efrain Ayllon on 8/7/16.
+//  Copyright © 2016 Efrain Ayllon. All rights reserved.
+//
+
+import UIKit
+
+class SuperchargerLocations: NSObject {
+    var nid :String!
+    var location_id :String!
+    var title :String!
+    var is_gallery :ObjCBool!
+    var sales_representative :ObjCBool!
+    var open_soon :String!
+    var address :String!
+    var address_line_1 :String!
+    var address_line_2 :String!
+    var address_notes :String!
+    var postal_code :String!
+    var city :String!
+    var province_state :String!
+    var country :String!
+    var latitude :String!
+    var longitude :String!
+    var region :String!
+    var sub_region :String!
+    var number :String!
+    var hours :String!
+    var chargers :String!
+    var amenities :String!
+}
--- Supercharged/ViewController.swift
+++ Supercharged/ViewController.swift
@@ -7,12 +7,85 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView :MKMapView!
+    var locations = [SuperchargerLocations]()
+    var locationManager :CLLocationManager!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadAPI()
+        }
+    
+    
+    private func loadAPI() {
+        
+        let myAPI = "https://www.tesla.com/all-locations?type=destination_charger"
+        
+        guard let url = NSURL(string: myAPI) else {
+            fatalError("Invalid URL")
+        }
+        
+        let session = NSURLSession.sharedSession()
+        
+        
+        session.dataTaskWithURL(url) { (data :NSData?, response :NSURLResponse?, error :NSError?) in
+            
+            guard let jsonResult = NSString(data: data!, encoding: NSUTF8StringEncoding) else {
+                fatalError("Unable to format data")
+            }
+            
+            
+            let superchargerResponse = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as! [NSDictionary!]
+            
+            
+            
+            for item in superchargerResponse {
+                let locations = SuperchargerLocations()
+                locations.nid = item.valueForKey("nid") as? String
+                locations.location_id = item.valueForKey("location_id") as? String
+                locations.title = item.valueForKey("title") as? String
+                locations.open_soon = item.valueForKey("open_soon") as? String
+                locations.address = item.valueForKey("address") as? String
+                locations.address_line_1 = item.valueForKey("address_line_1") as? String
+                locations.address_line_2 = item.valueForKey("address_line_2") as? String
+                locations.address_notes = item.valueForKey("address_notes") as? String
+                locations.postal_code = item.valueForKey("postal_code") as? String
+                locations.city = item.valueForKey("city") as? String
+                locations.province_state = item.valueForKey("province_state") as? String
+                locations.country = item.valueForKey("country") as? String
+                locations.latitude = item.valueForKey("latitude") as? String
+                locations.longitude = item.valueForKey("longitude") as? String
+                locations.region = item.valueForKey("region") as? String
+                locations.sub_region = item.valueForKey("sub_region") as? String
+                locations.number = item.valueForKey("number") as? String
+                locations.hours = item.valueForKey("hours") as? String
+                locations.chargers = item.valueForKey("chargers") as? String
+                locations.amenities = item.valueForKey("amenities") as? String
+                self.locations.append(locations)
+            }
+            print(self.locations)
+            dispatch_async(dispatch_get_main_queue(), {
+
+                for items in self.locations {
+                    print ("Lat: \(items.latitude), Long: \(items.longitude)")
+                    
+                    let pinAnnotation = MKPointAnnotation()
+                    pinAnnotation.title = items.title
+                    let myLat = Double(items.latitude)
+                    let myLong = Double(items.longitude)
+                    pinAnnotation.coordinate = CLLocationCoordinate2D(latitude: myLat!, longitude: myLong!)
+                    self.mapView.addAnnotation(pinAnnotation)
+                }
+            })
+            
+            }.resume()
     }
 
     override func didReceiveMemoryWarning() {
